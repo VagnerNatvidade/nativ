fb3cc68f651beb358cfff8bacfcfac6057addc41
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2025-03-15 03:04:27 -0300

    core: set the structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,107 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <header>
+        <h1>nativ</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <nav>
+          <Link href="#home">Home</Link>
+          <Link href="#about">Sobre</Link>
+          <Link href="#projects">Projetos</Link>
+        </nav>
+      </header>
+
+      <main>
+        <section id="home">
+          <h2>
+            Eleve seu negócio digital a outro nível
+            <span>
+              com um Front-end de qualidade!com um Front-end de qualidade!
+            </span>
+          </h2>
+
+          <p>
+            Olá! Sou Vagner Junior, desenvolvedor Front-end com especialidade em
+            React, HTML e CSS. Ajudo pequenos negócios e designers a colocarem
+            em prática boas ideias. Vamos conversar?
+          </p>
+        </section>
+
+        <section id="about">
+          <h2>
+            Sou Vagner <span>Jr!</span>
+          </h2>
+
+          <div>
+            <p>
+              Sou um desenvolvedor em formação apaixonado por tecnologia e
+              inovação. Estou me especializando em
+              <span>React, JavaScript, HTML e CSS</span>
+              para me tornar um Desenvolvedor Front-end e estou focado em
+              construir uma base sólida para desenvolver aplicações modernas e
+              responsivas.
+            </p>
+
+            <p>
+              Atualmente, estou aprofundando meus conhecimentos em React.js,
+              aprimorando minhas habilidades e buscando minha primeira
+              oportunidade como Dev Júnior. Acredito no aprendizado contínuo e
+              adoro desafios que me permitem crescer e evoluir na área. Se
+              quiser trocar uma ideia sobre tecnologia ou conhecer mais sobre
+              meu trabalho, ficarei feliz em conversar!
+            </p>
+          </div>
+
+          <Image
+            className="w-10"
+            src={"/me.jpg"}
+            alt="imagem de perfil do gituhb de Vagner"
+            width={1000}
+            height={1000}
+          />
+        </section>
+
+        <section id="projects">
+          <h2>Projetos</h2>
+
+          <article>
+            <Link href={""}>
+              <Image
+                className="w-10"
+                src={"/me.jpg"}
+                alt="imagem previwe do projeto xxx"
+                width={1000}
+                height={1000}
+              />
+
+              <div>
+                <h3>Nome do projeto</h3>
+                <p>descrição do projeto</p>
+              </div>
+            </Link>
+
+            <Link href={""}>
+              <Image
+                className="w-10"
+                src={"/me.jpg"}
+                alt="imagem previwe do projeto xxx"
+                width={1000}
+                height={1000}
+              />
+
+              <div>
+                <h3>Nome do projeto</h3>
+                <p>descrição do projeto</p>
+              </div>
+            </Link>
+          </article>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      <footer>Copyright (c) 2025 Vagner Junior</footer>
     </div>
   );
 }
