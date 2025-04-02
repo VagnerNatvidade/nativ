400c797a8f580da50beab28405141a57c6bf7449
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2025-04-01 23:41:39 -0300

    feat: ajusta footer layout

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full min-h-40 p-10 flex flex-col sm:flex-row sm:justify-between sm:items-start gap-3 items-center justify-center text-[#efefef] bg-[#04293a]">
-      <div>
-        <h2 className="border-b">Contatos</h2>
-        <nav className="flex flex-col mt-1">
+    <footer className="text-md w-full min-h-40 p-10 flex flex-col sm:flex-row sm:items-start gap-6 items-center justify-center text-[#efefef] bg-[#04293a]">
+      <div className="border-r pr-6">
+        <h2>Contatos</h2>
+        <nav className="flex flex-col mt-3 ">
           <Link href={"mailto:vagnerflorindojunior@gmai.com"}>Email</Link>
           <Link href={"https://www.instagram.com/vagner_natividade/"}>
             Instagram
